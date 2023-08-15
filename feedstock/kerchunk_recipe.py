f4dac5ba6ca2cf697eb842335c71b3e3b0af478c
briannapagan <briannapagan@gmail.com>
2023-08-15 11:15:41 -0400

    updating instructional files

--- feedstock/kerchunk_recipe.py
+++ feedstock/kerchunk_recipe.py
@@ -5,22 +5,32 @@ from pangeo_forge_recipes.transforms import OpenWithKerchunk, CombineReferences,
 from pangeo_forge_cmr import files_from_cmr
 
 
-# Get the GPM IMERG Late Precipitation Daily data
+# Specify the data collection shortname of your choice 
 shortname = 'GPM_3IMERGDL'
+# Specify the data collection version 
+version = '06'
 
-pattern = files_from_cmr( # Provide a list of files by querying CMR
+# Get a list of file URLs by querying CMR
+# Specify which dimension to chunk the data (concat_dim) and for that dimension
+# specify how many times that dimension occurs in the original files (nitems_per_file)
+pattern = files_from_cmr(
     shortname,
-    version='06',
+    version,
     nitems_per_file=1,
-    concat_dim='time',  # Describe how the dataset is chunked
+    concat_dim='time', 
 )
 
+# Grab netrc username and password
 (username, account,password) = netrc().authenticators("urs.earthdata.nasa.gov")
 client_kwargs = {
     "auth": aiohttp.BasicAuth(username, password),
     "trust_env": True,
 }
 
+# Run beam over the list of files/rechunk pattern defined above
+# Open the data with kerchunk if target files are kerchunk side car files
+# Call your EDL login information
+# Specify a kerchunk store name
 transforms = (
     beam.Create(pattern.items())
     | OpenWithKerchunk(
--- feedstock/zarr_recipe.py
+++ feedstock/zarr_recipe.py
@@ -5,22 +5,32 @@ from pangeo_forge_recipes.transforms import OpenURLWithFSSpec, OpenWithXarray, S
 from pangeo_forge_cmr import files_from_cmr
 
 
-# Get the GPM IMERG Late Precipitation Daily data
+# Specify the data collection shortname of your choice 
 shortname = 'GPM_3IMERGDL'
+# Specify the data collection version 
+version = '06'
 
-pattern = files_from_cmr( # Provide a list of files by querying CMR
+# Get a list of file URLs by querying CMR
+# Specify which dimension to chunk the data (concat_dim) and for that dimension
+# specify how many times that dimension occurs in the original files (nitems_per_file)
+pattern = files_from_cmr(
     shortname,
-    version='06',
+    version,
     nitems_per_file=1,
-    concat_dim='time',  # Describe how the dataset is chunked
+    concat_dim='time', 
 )
 
+# Grab netrc username and password
 (username, account,password) = netrc().authenticators("urs.earthdata.nasa.gov")
 client_kwargs = {
     "auth": aiohttp.BasicAuth(username, password),
     "trust_env": True,
 }
 
+# Run beam over the list of files/rechunk pattern defined above
+# Call your EDL login information
+# Open the data with xarray if target file output is zarr
+# Specify a zarr store name
 transforms = (
     beam.Create(pattern.items())
     | OpenURLWithFSSpec(
@@ -28,7 +38,7 @@ transforms = (
     )
     | OpenWithXarray(file_type=pattern.file_type)
     | StoreToZarr(
-        store_name = 'gpm.zarr', # wherever it goes this is what the name of the file will be
+        store_name = 'gpm.zarr', 
         combine_dims = pattern.combine_dim_keys,
     )
 )
